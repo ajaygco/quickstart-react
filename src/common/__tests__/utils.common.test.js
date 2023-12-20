4789c71cc2db178d92f46bb47eccab13b6b193ea
Ajay Gupta <itsajayg@icloud.com>
2023-12-20 13:06:13 +0530

    setup unit tests

---
+++ src/common/__tests__/utils.common.test.js
@@ -0,0 +1,11 @@
+// yarn test source/common/__tests__/utils.common.test.js
+
+import { getUtcTimestamp } from "common/utils.common";
+
+describe("utils.common", () => {
+  describe("getUtcTimestamp", () => {
+    it("should return a number", () => {
+      expect(typeof getUtcTimestamp()).toBe("number");
+    });
+  });
+});
---
+++ src/components/shared/__tests__/logo.shared.test.jsx
@@ -0,0 +1,14 @@
+// yarn test source/components/shared/__tests__/logo.shared.test.jsx
+
+import React from "react";
+import { render } from "@testing-library/react";
+
+import { Logo } from "components/shared/logo.shared";
+
+describe("Logo", () => {
+  it("should render correctly", () => {
+    const wrapper = render(<Logo />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
